746ef5a5f3d4cfb4339375bd83f729352c9d4ebb
tancodegym@gmail.com <tancodegym@gmail.com>
2021-08-16 10:52:24 +0700

    bai1 module2

---
+++ module2/src/_01_bai1/thuc_hanh/system_time.java
@@ -0,0 +1,10 @@
+package _01_bai1.thuc_hanh;
+
+import java.util.Date;
+
+public class system_time {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is : " +now);
+    }
+}
---
+++ module2/src/_01_bai1/thuc_hanh/tinh_chi_so_can_nang_cua_co_the.java
@@ -0,0 +1,24 @@
+package _01_bai1.thuc_hanh;
+
+import java.util.Scanner;
+
+public class tinh_chi_so_can_nang_cua_co_the {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight,height,bmi;
+        System.out.print("Nhập vào số cân của bạn(kg)");
+        weight=scanner.nextDouble();
+        System.out.print("Nhập vào chiều cao của bạn(m)");
+        height=scanner.nextDouble();
+        bmi=weight/Math.pow(height, 2);
+        System.out.printf("%-20s%s" ,"bmi", "Tình trạng\n");
+        if (bmi < 18)
+            System.out.printf("%-20.2f%s", bmi, "Thiếu cân");
+        else if (bmi < 25.0)
+            System.out.printf("%-20.2f%s", bmi, "Bình thường");
+        else if (bmi < 30.0)
+            System.out.printf("%-20.2f%s", bmi, "Dư cân");
+        else
+            System.out.printf("%-20.2f%s", bmi, "Béo phì");
+    }
+}
