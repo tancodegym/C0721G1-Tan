833a3cb3e4bc7044b364678b9284449c2095a427
tancodegym@gmail.com <tancodegym@gmail.com>
2021-08-19 14:14:08 +0700

    up Bai tap bai 5

---
+++ module2/src/_05_bai5_AccessModifier/bai_tap/AccessModifier.java
@@ -0,0 +1,35 @@
+package _05_bai5_AccessModifier.bai_tap;
+
+import java.util.Scanner;
+
+public class AccessModifier {
+    static class Circle {
+        private double radius=1.0;
+        private String color="red";
+        Circle(){
+
+        }
+        Circle(double r){
+            this.radius=r;
+        }
+        protected   double getRadius(){
+            return  this.radius;
+        }
+        protected double getArea(){
+            return this.radius*this.radius*Math.PI;
+        }
+
+    }
+    static class TestCircle {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter number of radius: ");
+            double r=Double.parseDouble(input.nextLine());
+            Circle circle= new Circle(r);
+            double area = circle.getArea();
+            System.out.println("Area of circle is: " +area);
+            System.out.println("Radius of circle is: " +circle.getRadius());
+        }
+
+    }
+}
---
+++ module2/src/_05_bai5_AccessModifier/bai_tap/XayDungLopChiGhiTrongJava.java
@@ -0,0 +1,62 @@
+package _05_bai5_AccessModifier.bai_tap;
+
+import java.util.Scanner;
+
+
+class Student {
+    private String name;
+    private String classes;
+
+    Student() {
+        this.name = "John";
+        this.classes = "C02";
+    }
+
+    void setName(String name) {
+        this.name = name;
+
+    }
+
+    void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    void getName() {
+        System.out.println(name);
+    }
+
+    void getClasses() {
+        System.out.println(classes);
+    }
+
+}
+
+class Test {
+    public static void main(String[] args) {
+        Student studentA = new Student();
+        Student studentB = new Student();
+        studentA.getClasses();
+        studentA.getName();
+        studentB.getClasses();
+        studentB.getName();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter name of student A: ");
+        String nameOfStudentA = input.nextLine();
+        System.out.println("Enter name of student B: ");
+        String nameOfStudentB = input.nextLine();
+        System.out.println("Enter classes of student A: ");
+        String classesOfStudentA = input.nextLine();
+        System.out.println("Enter classes of student B: ");
+        String classesOfStudentB = input.nextLine();
+        studentA.setName(nameOfStudentA);
+        studentB.setName(nameOfStudentB);
+        studentA.setClasses(classesOfStudentA);
+        studentB.setClasses(classesOfStudentB);
+        studentA.getClasses();
+        studentA.getName();
+        studentB.getClasses();
+        studentB.getName();
+
+    }
+}
+
